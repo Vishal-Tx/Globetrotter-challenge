400d96e5bc9deebe0f324f46ab95c41489e38a17
Vishal-Tx <vishalguptaa2z111@gmail.com>
2025-03-08 14:16:00 +0530

    add unused route error to send on FE

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,10 @@ mongoose.connect(process.env.MONGO_URL)
    .catch(err => console.error(err));
 
 app.use("/api", routes);
+app.use((req, res, next) => {
+   res.status(404).json({
+      success: false,
+      message: "Route not found"
+   });
+});
 app.listen(5000, () => console.log("Server running on port 5000"));
