6073a456534058fb39cdd6d5a0e45c8a4277e933
Vishal-Tx <vishalguptaa2z111@gmail.com>
2025-03-08 23:44:40 +0530

    add assets

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -2,4 +2,8 @@
 import SadIcon from './sadIcon.svg?react';
 import SmilyIcon from './smileyIcon.svg?react';
 import GlobeIcon from './globe.svg?react';
-export { SadIcon, SmilyIcon, GlobeIcon };
+import GuardX from './guardx.svg?react';
+import GuardT from './guardt.svg?react';
+import GuardS from './guards.svg?react';
+import GuardO from './guardo.svg?react';
+export { SadIcon, SmilyIcon, GlobeIcon, GuardO, GuardS, GuardT, GuardX };
